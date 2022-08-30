0af9ff57ad400870ee65fe3fb43b4e1bfe1edf6d
Re-Dnor <MokshinRE@yandex.ru>
2022-08-30 18:43:43 +0500

    create information for github user

--- src/Components/Info.tsx
+++ src/Components/Info.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
+import Profile from './Profile';
 
 function Info() {
+  const { selected } = useSelector((state: RootState) => state.github);
   return (
-    <div>s</div>
+    <div>
+      {
+      selected
+      && <Profile />
+      }
+    </div>
   );
 }
 
