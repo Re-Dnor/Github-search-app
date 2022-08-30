3671446b2325405efb9813e6a38fb086d0e034c3
Re-Dnor <MokshinRE@yandex.ru>
2022-08-30 18:43:18 +0500

    add wrapper

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Container } from '@mui/material';
 
 import SearchForm from './SeacrhForm';
 import Info from './Info';
 
 function App() {
   return (
-    <>
+    <Container component="main" maxWidth="xl">
       <SearchForm />
       <Info />
-    </>
+    </Container>
   );
 }
 
